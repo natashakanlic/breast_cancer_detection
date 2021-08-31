746a505d9e3e75025395a9bf8800f00b905a7dbb
natashakanlic <57207973+natashakanlic@users.noreply.github.com>
2021-08-31 13:26:25 -0700

    added dataset, data_processing splits data and trains

--- src/data_processing.py
+++ src/data_processing.py
@@ -3,35 +3,59 @@
 '''
 3-dimensional space - each row is a description of a cell nucleus in three dimensions
 
-radius1 = mean radius
-radius2 = radius SE
-radius3 = worst radius
+radius_mean = mean radius
+radius_SE = radius SE
+radius_worst = worst radius
 '''
 
 import csv
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-'''
-data = pd.read_csv('../dataset/breast-cancer-wisconsin.data')
-print("data")
-print(data)
 
-data2 = pd.read_csv('../dataset/Index')
-print("data2")
-print(data2)
+from sklearn.pipeline import make_pipeline
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-data4 = pd.read_csv('../dataset/wpbc.data')
-print("data4")
-print(data4)
-'''
+def main():
+    '''
+    data = pd.read_csv('../dataset/breast-cancer-wisconsin.data')
+    print("data")
+    print(data)
 
-header_list = ['id', 'diagnosis', 'radius1', 'texture1', 'perimeter1', 'area1', 'smoothness1',
-               'compactness1', 'concavity1', 'concave-points1', 'symmetry1', 'fractal-dimension1', 'radius2', 'texture2', 'perimeter2', 'area2', 'smoothness2',
-               'compactness2', 'concavity2', 'concave-points2', 'symmetry2', 'fractal-dimension2', 'radius3', 'texture3', 'perimeter3', 'area3', 'smoothness3',
-               'compactness3', 'concavity3', 'concave-points3', 'symmetry3', 'fractal-dimension3']
-data = pd.read_csv('../dataset/wdbc.data',
-                   names=header_list)
-print(data)
-# print(data['id'])  # column id
-# print(data.iloc[1]) #row 1
+    data2 = pd.read_csv('../dataset/Index')
+    print("data2")
+    print(data2)
+
+    data4 = pd.read_csv('../dataset/wpbc.data')
+    print("data4")
+    print(data4)
+    '''
+
+    header_list = ['id', 'diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean',
+                'compactness_mean', 'concavity_mean', 'concave-points_mean', 'symmetry_mean', 'fractal-dimension_mean', 'radius_SE', 'texture_SE', 'perimeter_SE', 'area_SE', 'smoothness_SE',
+                'compactness_SE', 'concavity_SE', 'concave-points_SE', 'symmetry_SE', 'fractal-dimension_SE', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst',
+                'compactness_worst', 'concavity_worst', 'concave-points_worst', 'symmetry_worst', 'fractal-dimension_worst']
+    data = pd.read_csv('../dataset/wdbc.data',
+                    names=header_list)
+    print(data)
+    # print(data['id'])  # column id
+    # print(data.iloc[1]) #row 1
+
+
+    # Splitting data
+    X = data.iloc[:, 2:].values
+    y = data['diagnosis'].values
+    X_train, X_valid, y_train, y_valid = train_test_split(
+        X, y, test_size=0.21, random_state=42)
+
+    model = make_pipeline(StandardScaler(), RandomForestClassifier(n_estimators=300, max_depth=10, min_samples_leaf=3))
+    model.fit(X_train, y_train)
+    print(model.score(X_valid, y_valid))
+
+if __name__ == '__main__':
+    main()
